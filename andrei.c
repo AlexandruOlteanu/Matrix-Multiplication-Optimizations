c982d7568d437bcb99a111b093a87364c5833eba
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2023-05-06 13:31:35 +0300

    Starting Point

--- andrei.c
+++ andrei.c
@@ -1,47 +1,60 @@
-/*
- * Tema 2 ASC
- * 2023 Spring
- */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <cblas.h>
+double* my_solver3(int N, double *A, double* B) {
+     printf("NEOPT SOLVER\n");
+    double *C = (double*)malloc(N * N * sizeof(double));
+    memset(C, 0, N * N * sizeof(double));
+    double sum;
+    int i, j, k;
 
-#define DIE(condition, message) \
-    if (condition) {            \
-        perror(message);        \
-        exit(EXIT_FAILURE);     \
+    // Perform matrix multiplication of A and B
+    for (i = 0; i < N; i++) {
+        // Loop over columns of B
+        for (j = 0; j < N; j++) {
+            sum = 0.0;
+            // Only multiply elements above or on the diagonal of A
+            for (k = i; k < N; k++) {
+                sum += A[i * N + k] * B[k * N + j];
+            }
+            // Store result in C
+            C[i * N + j] = sum;
+        }
     }
-double* my_solver(int N, double *A, double *B) {
-    printf("BLAS SOLVER\n");
 
-    // Calculate B x A
-    double *BxA = calloc(N * N, sizeof(double));
-    DIE(BxA == NULL, "BxA calloc error");
+    // Multiply C with transpose of A
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            sum = 0.0;
+            // Multiply corresponding elements of C and A transpose
+            for (k = j; k < N; k++) {
+                sum += C[i * N + k] * A[j * N + k];
+            }
+            // Store result in C
+            C[i * N + j] = sum;
+        }
+    }
+    
+    // Multiply transpose of B with itself
+    double *D = (double*)malloc(N * N * sizeof(double));
+  //  memset(D, 0, N * N * sizeof(double));
+    for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++) {
+                sum = 0.0;
+                // Multiply corresponding elements of transpose of B and B
+                for (k = 0; k < N; k++) {
+                    sum += B[k * N + i] * B[j * N + k ];
+                }
+                // Store result in D
+                D[i * N + j] = sum;
+            }
+        }
 
-    // Initialize BxA = B, after multiplication, BxA = B x A
-    memcpy(BxA, B, N * N * sizeof(double));
+    // Add D to C and return the result
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            C[i * N + j] += D[i * N + j];
+        }
+    }
 
-    cblas_dtrmm(CblasRowMajor, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, N, N, 1, A, N, BxA, N);
-
-    // Calculate BxA x A^T
-    double *result = calloc(N * N, sizeof(double));
-    DIE(result == NULL, "Result calloc error");
-
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1, BxA, N, A, N, 0, result, N);
-
-    // Calculate B transpose x B
-    double *BtB = calloc(N * N, sizeof(double));
-    DIE(BtB == NULL, "BtB calloc error");
-
-    cblas_dgemm(CblasRowMajor, CblasTrans, CblasTrans, N, N, N, 1, B, N, B, N, 0, BtB, N);
-
-    // Add BtB to the result
-    cblas_daxpy(N * N, 1, BtB, 1, result, 1);
-
-    // Free memory allocated for BxA and BtB
-    free(BxA);
-    free(BtB);
-
-    return result;
+    
+    free(D); // free memory allocated for D
+    return C;
 }
