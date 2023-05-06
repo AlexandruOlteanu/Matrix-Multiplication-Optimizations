4d4f2d7425280b56ad8e20d2b055f161c9716bd4
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2023-05-06 14:35:41 +0300

    Starting Point

--- solver_opt.c
+++ solver_opt.c
@@ -1,73 +1,94 @@
-/*
- * Tema 2 ASC
- * 2023 Spring
- */
 #include "utils.h"
+#include <string.h>
 
-/*
- * Add your optimized implementation here
- */
-double* my_solver(int N, double *A, double* B) {
-     printf("NEOPT SOLVER\n");
-    double *C = (double*)malloc(N * N * sizeof(double));
-    memset(C, 0, N * N * sizeof(double));
-    double sum;
-    int i, j, k;
+#define ERROR(message) { \
+        fprintf(stderr, "An Error has occured with message: %s\n", message); \
+        exit(EXIT_FAILURE); \
+}
 
-    // Perform matrix multiplication of A and B
-    for (i = 0; i < N; i++) {
-        // Loop over columns of B
-        for (j = 0; j < N; j++) {
-            sum = 0.0;
-            // Only multiply elements above or on the diagonal of A
-            for (k = i; k < N; k++) {
-                sum += A[i * N + k] * B[k * N + j];
-            }
-            // Store result in C
-            C[i * N + j] = sum;
-        }
+#define ZERO 0
+#define double_sz sizeof(double)
+
+double* my_solver(int sz, double *a, double* b) {
+    printf("OPT SOLVER\n");
+    double *c = (double*)calloc(sz * sz, sizeof(double));
+
+    if (c == NULL) {
+        ERROR("Calloc failed");
     }
 
-    // Multiply C with transpose of A
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            sum = 0.0;
-            // Multiply corresponding elements of C and A transpose
-            for (k = j; k < N; k++) {
-                sum += C[i * N + k] * A[j * N + k];
-            }
-            // Store result in C
-            C[i * N + j] = sum;
-        }
-    }
-
-    // Compute transpose of B
-       
-
-
-    // Multiply transpose of B with itself
-    double *D = (double*)malloc(N * N * sizeof(double));
-  //  memset(D, 0, N * N * sizeof(double));
-    for (i = 0; i < N; i++) {
-            for (j = 0; j < N; j++) {
-                sum = 0.0;
-                // Multiply corresponding elements of transpose of B and B
-                for (k = 0; k < N; k++) {
-                    sum += B[k * N + i] * B[j * N + k ];
-                }
-                // Store result in D
-                D[i * N + j] = sum;
-            }
-        }
-
-    // Add D to C and return the result
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            C[i * N + j] += D[i * N + j];
-        }
-    }
-
+    register int i = ZERO, j = ZERO, k = ZERO;
+    register double result = 0;
     
-    free(D); // free memory allocated for D
-    return C;
+    for (i = 0; i < sz; ++i) {
+        for (j = 0; j < sz; ++j) {
+            c[j + i * sz] = ZERO;
+        }
+    }
+
+    i = ZERO;
+    do {
+        j = ZERO;
+        do {
+            result = ZERO;
+            k = i;
+            do {
+                result += a[k + i * sz] * b[j + k * sz];
+                ++k;
+            } while (k < sz);
+            c[j + i * sz] = result;
+            ++j;
+        } while (j < sz);
+        ++i;
+    } while (i < sz);
+
+    i = ZERO;
+    do {
+        j = ZERO;
+        do {
+            result = ZERO;
+            k = ZERO;
+            do {
+                result += c[k + i * sz] * a[k + j * sz];
+                ++k;
+            } while (k < sz);
+            c[j + i * sz] = result;
+            ++j;
+        } while (j < sz);
+        ++i;
+    } while (i < sz);
+
+    double *second = calloc(sz * sz, double_sz);
+    if (second == NULL) {
+        ERROR("Calloc failed");
+    }
+    i = ZERO;
+    do {
+        j = ZERO;
+        do {
+            k = ZERO;
+            result = 0;
+            do {
+                result += b[k * sz + i] * b[j * sz + k];
+                ++k;
+            } while (k < sz);
+            second[j + i * sz] = result;
+            ++j;
+        } while (j < sz);
+        ++i;
+    } while (i < sz);
+
+    i = ZERO;
+    do {
+        j = ZERO;
+        do {
+            c[j + i * sz] += second[j + i * sz];
+            ++j;
+        } while (j < sz);
+        ++i;
+    } while (i < sz);
+
+
+    free(second);
+    return c;
 }
